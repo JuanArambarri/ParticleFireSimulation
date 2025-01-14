3bee0e29c49111bea228d635443b80805f100725
Juan Bautista Arambarri <jbat1997@gmail.com>
2025-01-14 08:59:00 -0300

    Implement Screen class
    Screen class has been implemented and appropriate functions have been refactored to this class. Whole screen can now be changed to another color.

---
+++ Screen.h
@@ -0,0 +1,33 @@
+//
+// Created by bauli on 2025/01/14.
+//
+
+#ifndef PARTICLEFIRESIMULATION_SCREEN_H
+#define PARTICLEFIRESIMULATION_SCREEN_H
+#include <iostream>
+#include <SDL.h>
+
+
+namespace Juan {
+
+    class Screen {
+    public:
+        const static int SCREEN_WIDTH = 800;
+        const static int SCREEN_HEIGHT = 600;
+    private: //if member variables, prefix with m_ (instance variables vs local variables)
+        SDL_Window *m_window;
+        SDL_Renderer *m_renderer;
+        SDL_Texture *m_texture;
+        Uint32 *m_buffer;
+
+    public:
+        Screen();
+        bool init(); //check if fail
+        bool processEvents();
+        void close();
+
+    };
+
+} // Juan
+
+#endif //PARTICLEFIRESIMULATION_SCREEN_H
--- main.cpp
+++ main.cpp
@@ -1,64 +1,27 @@
 #include <iostream>
 #include <SDL.h>
+#include "Screen.h"
 
 using namespace std;
+using namespace Juan;
 
 int main(int argc, char* argv []) {
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    if (SDL_Init(SDL_INIT_VIDEO) <0){
-        cout << "SDL Init failed." << endl;
-        return 1;
+
+    Screen screen;
+
+    if (screen.init() == false) {
+        cout << "Error initialising SDL." << endl;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL){
-        SDL_Quit();
-        return 2;
-    }
-
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-    SDL_Texture *texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STATIC,SCREEN_WIDTH,SCREEN_HEIGHT); //destroy texture first
-    if (renderer == NULL){
-        cout << "Could not create renderer" << endl;
-        SDL_DestroyWindow(window); // Will refactor later due to repetitive blocks of code
-        SDL_Quit();
-        return 3;
-    }
-    if (texture == NULL){
-        cout << "Could not create texture" << endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 4;
-    }
-    Uint32 *buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT]; //remember to free, might be garbage and show something.
-
-    memset(buffer, 0xFF,SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32)); //255 is white (Sets whole screen to white)
-
-    SDL_UpdateTexture(texture, NULL, buffer,SCREEN_WIDTH * sizeof(Uint32));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer,texture,NULL, NULL);
-    SDL_RenderPresent(renderer);
-
-    bool quit = false;
-
-    SDL_Event event;
-
-    while(!quit){
+    while(true){
         //update particles
         //draw particles
         //check for messages/events
-        while(SDL_PollEvent(&event)){
-            if (event.type == SDL_QUIT){
-                quit = true;
-            }
+        if(!screen.processEvents()){
+            break;
         }
+
     }
-    delete [] buffer;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    screen.close();
     return 0;
 }
